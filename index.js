db940957153ab079c30856b9ab6e1ed3f983c958
Mark Shkreli <m.shkreli96@icloud.com>
2022-09-14 17:23:00 -0400

    commit tree fc0d75e752d72aedf434c56a3d2f1cb96cf688c5

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee ={
+    name : "Sam",
+    streetAddress : "11 Broadway Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, streetAddress , value){
+    const newEmployee = {...employee};
+    newEmployee[streetAddress] = "11 Broadway"
+
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+    employee[streetAddress] = "12 Broadway";
+
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, name, value) {
+    const newEmployee = {...employee};
+     delete newEmployee.name;
+
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, name, value) {
+    delete employee.name;
+
+    return employee;
+};
